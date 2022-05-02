d4c23929b13fc2aa261c6b8f09978567a08f4bbd
Thomas Brand <thomas.brand@sou.inteli.edu.br>
2022-05-02 09:04:59 -0300

    final

---
+++ 03_AUT_EST_ENTREGA/Semana 2/teste.js
@@ -0,0 +1,22 @@
+const http = require('http'); 
+const fs = require('fs');
+const hostname = '127.0.0.1'; 
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('index.html', null,  (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.write('Arquivo não encontrado');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+  });
+
+server.listen(port, hostname, () => { 
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
